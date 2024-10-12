81efd60c354d88cd8efc2624cecfa798322555d3
pafsharb <pa413@kent.ac.uk>
2024-10-12 15:44:23 +0000

    part 7

--- partSeven.py
+++ partSeven.py
@@ -1,3 +1,22 @@
-num1 = int(input("Choose a number."))
-num2 = int(input("choose a number."))
+num1 = float(input("Choose a number."))
+num2 = float(input("Choose a number."))
+operation = input("Choose an operation.")
 
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    result = num1/num2
+    print(f"{num1} / {num2} = {result}")
+elif operation == "^":
+    result = num1^num2
+    print(f"{num1} ^ {num2} = {result}")
+elif operation == "%":
+    result = num1 % num2
+    print(f"{num1} % {num2} = {result}")
